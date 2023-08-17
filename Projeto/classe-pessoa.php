0bad60fc3942c4c97e92cef7270228bd643fe9a4
SabSVL <102835900+SabSVL@users.noreply.github.com>
2023-08-17 10:02:54 -0300

    projeto terminado

--- Projeto/classe-pessoa.php
+++ Projeto/classe-pessoa.php
@@ -1,27 +1,28 @@
 <?php
 
-Class Pessoa {
-private $pdo;
+class Pessoa
+{
+    private $pdo;
 
-    public function __construct($dbname, $host, $user, $senha){
+    public function __construct($dbname, $host, $user, $senha)
+    {
 
-        try{
-            $this-> pdo = new PDO("mysql:dbname=".$dbname. ";host=" . $host, $user, $senha);
-
-
-        }catch (PDOException $e){
-            echo "Erro com banco de dados:" .$e->getMessage();
-           exit();
-        }catch (Exception $e) {
-            echo "Erro generico: " .$e->GetMessage();
+        try {
+            $this->pdo = new PDO("mysql:dbname=" . $dbname . ";host=" . $host, $user, $senha);
+        } catch (PDOException $e) {
+            echo "Erro com banco de dados:" . $e->getMessage();
+            exit();
+        } catch (Exception $e) {
+            echo "Erro generico: " . $e->GetMessage();
             exit();
         }
     }
     //FUNÇÃO PARA BUSCAR OS DADOS E COLOCAR NO CONTA DIRETO/ TABELA
-    public function buscarDados(){
+    public function buscarDados()
+    {
         $res = array();
         $cmd = $this->pdo->query("SELECT * FROM pessoa ORDER BY nome");
-        $res = $cmd-> fetchAll(PDO::FETCH_ASSOC);
+        $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
         return $res;
     }
 
@@ -32,27 +33,51 @@ private $pdo;
         $cmd = $this->pdo->prepare("SELECT id FROM pessoa WHERE email = :e");
         $cmd->bindValue(":e", $email);
         $cmd->execute();
-        if($cmd-> rowCount() > 0) // verifcar se o email ja existe no banco de dados
+        if ($cmd->rowCount() > 0) // verifcar se o email ja existe no banco de dados
         {
             return false;
-
-        }else{ // não foi encontrado
+        } else { // não foi encontrado
             $cmd = $this->pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:n, :t, :e)");
             $cmd->bindValue(":n", $nome);
             $cmd->bindValue(":t", $telefone);
             $cmd->bindValue(":e", $email);
             $cmd->execute();
             return true;
-
         }
     }
-    public function excluirPessoa($id) {
+
+    //FUNÇÃO PARA EXCLUIR PESSOA
+    public function excluirPessoa($id)
+    {
         $cmd = $this->pdo->prepare("DELETE FROM pessoa WHERE id = :id");
         $cmd->bindValue(":id", $id);
         $cmd->execute();
+    }
+
+    //FUNÇÃO PARA BUSCAR OS DADOS
+    public function buscarDadosPessoa($id)
+    {
+        $res = array();
+        $cmd = $this->pdo->prepare("SELECT * FROM pessoa WHERE id = :id");
+        $cmd->bindValue(":id", $id);
+        $cmd->execute();
+        $res = $cmd->fetch(PDO::FETCH_ASSOC);
+        return  $res;
+    }
 
 
+    // FUNÇÃO PARA ATUALIZAR OS DADOS NO BANCO DE DADOS
+    public function atualizarDados($id, $nome, $telefone, $email)
+    {
+        
+            $cmd = $this->pdo->prepare("UPDATE pessoa SET nome = :n, telefone =:t, email = :e WHERE id = :id");
+
+            $cmd->bindValue(":id", $id);
+            $cmd->bindValue(":n", $nome);
+            $cmd->bindValue(":t", $telefone);
+            $cmd->bindValue(":e", $email);
+            $cmd->execute();
+            return true;
+   
     }
 }
-
-?>
