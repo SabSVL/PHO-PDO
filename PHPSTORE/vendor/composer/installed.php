9bc9a46f31de228de746e5f4988bf56bd2e644d1
SabSVL <102835900+SabSVL@users.noreply.github.com>
2023-09-17 21:11:38 -0300

    Add files via upload

---
+++ PHPSTORE/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/store',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b35d34e436bd4f445ba0ae8c8f57c921d07bb2d8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/store' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b35d34e436bd4f445ba0ae8c8f57c921d07bb2d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
