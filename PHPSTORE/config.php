bd3c9f9e52cdc84f2b7d2923833fce7826fd0a52
SabSVL <102835900+SabSVL@users.noreply.github.com>
2023-09-17 21:10:08 -0300

    Add files via upload

---
+++ PHPSTORE/config.php
@@ -0,0 +1,22 @@
+<?php
+
+define('APP_NAME', 'PHPSTORE');
+define('APP_VERSION', '1.0.0');
+
+define('BASE_URL', 'http://localhost/PHO-PDO/PHPSTORE/public/');
+
+// MYSQL
+define('MYSQL_SERVER','localhost' );
+define('MYSQL_DATABASE','php_store' );
+define('MYSQL_USER', 'root');
+define('MYSQL_PASS', "");
+define('MYSQL_CHARSET', 'utf8');
+
+
+// mail
+
+define('EMAIL_HOST', 'smtp.gmail.com');
+define('EMAIL_FROM', 'sabrinaluiz6789@gmail.com');
+define('EMAIL_PASS', 'ufditklyuhwlqstk');
+define('EMAIL_PORT', 465);
+?>
